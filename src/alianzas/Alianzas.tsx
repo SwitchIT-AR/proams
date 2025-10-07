3ed8d59586915b207d1c7d8c9a1c799de0be9c37
matieckhardt <matias.eckhardt@gmail.com>
2025-10-07 19:07:14 -0300

    fix alianzas column revers

--- src/alianzas/Alianzas.tsx
+++ src/alianzas/Alianzas.tsx
@@ -1,19 +1,27 @@
 import {
   Flex,
   Title,
+  Box,
   Text,
   Image,
   Stack,
-  Box
 } from "@mantine/core";
 import alianzas from "../assets/alianzas.png";
 
 export function Alianzas() {
   return (
     <Box bg={"#F0F0F0"} pt={{ base: 70, sm: 0 }}>
-      <Flex direction={{ base: "column", md: "row" }} gap={0}>
-        {/* Texto: primero en todas las vistas */}
-        <Box w={{ base: "100%", md: "50%" }} pt={{ base: 10, sm: 100 }} pb={{ base: 100, sm: 0 }}>
+      <Flex
+        direction={{ base: "column-reverse", md: "row" }}
+        gap={0}
+      >
+        {/* Imagen: en mobile va abajo, en desktop a la izquierda */}
+        <Box w={{ base: "100%", md: "50%" }}>
+          <Image src={alianzas} w={"100%"} />
+        </Box>
+
+        {/* Texto: en mobile va arriba, en desktop a la derecha */}
+        <Box w={{ base: "100%", md: "50%" }} mt={{ base: 10, sm: 140 }} pb={100}>
           <Stack
             gap={"xl"}
             justify="center"
@@ -23,15 +31,11 @@ export function Alianzas() {
           >
             <Title c={"#1A3B6D"}>Alianzas</Title>
             <Text size={"1.3em"}>
-              Establecimos <strong>alianzas estratégicas con astilleros</strong> de la región en
-              rubros como <strong>diseño, construcción y reparación de buques.</strong>
+              Establecimos <strong>alianzas estratégicas con astilleros</strong> de la
+              región en rubros como{" "}
+              <strong>diseño, construcción y reparación de buques.</strong>
             </Text>
           </Stack>
-        </Box>
-
-        {/* Imagen: derecha en desktop, abajo en mobile */}
-        <Box w={{ base: "100%", md: "50%" }}>
-          <Image src={alianzas} fit="cover" w={"100%"} />
         </Box>
       </Flex>
     </Box>
